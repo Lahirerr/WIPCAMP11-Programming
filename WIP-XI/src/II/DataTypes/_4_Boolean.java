4f1758c92349cdb73d3f699fb6537df1e18c19fc
Lahierr <m_tanachart@hotmail.com>
2019-05-31 08:43:47 +0700

    Add project

---
+++ WIP-XI/src/II/DataTypes/_4_Boolean.java
@@ -0,0 +1,29 @@
+/*
+ * WIP XI Computer Programing 
+ */
+
+package II.DataTypes;
+
+/**
+ *
+ * @author sittiwatlcp
+ */
+public class _4_Boolean {
+    public static void main(String[] args) {
+        
+        boolean Online = true;
+        boolean Sleep = false;
+        boolean Walking = false;
+        boolean Flying = false;
+        
+        System.out.println("is Online ? : " + Online);
+        System.out.println("is Sleeping ? : " + Sleep);
+        System.out.println("is Walking ? : " + Walking);
+        System.out.println("is Flying ? :" +  Flying);
+        
+        Online = !true;
+        
+        System.out.println("is Online ? :" + Online);
+        
+    }
+}
